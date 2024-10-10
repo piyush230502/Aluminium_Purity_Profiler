e63e2d878e231c4d85691505b5f58febfdf20a13
piyush230502 <piyush230502@gmail.com>
2024-10-10 12:50:02 +0530

    first commit

---
+++ src/constant/__init__.py
@@ -0,0 +1,14 @@
+import os
+
+
+AWS_S3_BUCKET_NAME = "Aluminium-quality"
+MONGO_DATABASE_NAME = "pwskills"
+MONGO_COLLECTION_NAME = "Aluminium"
+
+TARGET_COLUMN = "rod_quality"
+MONGO_DB_URL="mongodb+srv://piyush:12345@aluminium.vlfws.mongodb.net/?retryWrites=true&w=majority&appName=Aluminium"
+
+MODEL_FILE_NAME = "model"
+MODEL_FILE_EXTENSION = ".pkl"
+
+artifact_folder =  "artifacts"
